08d1bb20170f6af827123fbcbce4b1674c4b8aa1
vlada270 <55885082+vlada270@users.noreply.github.com>
2020-02-14 14:19:23 +0300

    Update print.c

--- print.c
+++ print.c
@@ -43,13 +43,109 @@ int find_symbol(char *str, int start) //реализовать %%
     }
     return (-1);
 }
-void ft_int(const char * format, char *str, va_list ap) // скорее всего, нужно сделать ap через указатели, чтобы она изменялась
+int ft_help(char *str, int x)
 {
+    int start;
+    int finish;
+
+    start = ft_strchr(str, x) - str + 1;
+    finish = ft_strrchr(str, x) - str + 1;
+    return(finish - start);
+}
+
+char *ft_help_2(int len, char c)
+{
+    char *str;
+    int i;
+
+    i = 0;
+    str = ft_memalloc(len); //освободить память
+    while (i < len)
+    {
+        str[i] = c;
+        i++;
+    }
+    return (str);
+}
+
+char * ft_search_length(char *str, int len)
+{
+    if (len > 1)
+    {
+        if (str[len - 1] == 'h')
+        {
+            if (ft_help(str, 'h') != 0)
+                return(ft_help_2(2, 'h')); //освободить память
+            else
+               return(ft_help_2(1, 'h')); 
+        }
+        else if (str[len - 1] == 'l')
+        {
+            if (ft_help(str, 'l') != 0) 
+               return(ft_help_2(2, 'l')); 
+            else
+               return(ft_help_2(1, 'l')); 
+        }
+    }
+    return (NULL);
+}
+
+int ft_search_length_dop(char *str, int len)
+{
+    if (len > 1)
+    {
+        if (str[len - 1] == 'h')
+        {
+            if (ft_help(str, 'h') != 0)
+                return(1); //hh
+            else
+               return(2); //h
+        }
+        else if (str[len - 1] == 'l')
+        {
+            if (ft_help(str, 'l') != 0) 
+               return(3); // ll
+            else
+               return(4); // l
+        }
+    }
+    return (0);
+}
+
+char *ft_formatting(int length, char str, va_list ap, int len)
+{
+    signed char hh;
+    short int h;
+    long int l;                     //нужно узнат есть ли в дальнейшем разница при преобразованиях 
+    long long int ll;
     int x;
 
-    x = va_arg(ap, int); //далее нам нужно распарсить str, чтобы узнать флаги и применить их 
-    printf("%d", x);
+    if (lenghth == 0)
+    {
 
+    }
+
+
+}
+
+
+void ft_int(const char * format, char *str, va_list ap) // можно сделать функцию поиска str в общем предложении и заменить ее на то, что получится от преобразования и вернуть строчку 
+{
+    int x;
+    int length;
+    int len;
+    char *s;
+
+    //реализовать функцию, которая поможет узнать lenght
+    len = (int)ft_strlen(str) - 1;
+    length = ft_search_length_dop(str, len);
+    //найдя длину мы посылаем в следующую функцию где преобразовываем под эту длину и делаем преобразования, сюда возвращаем то, что получилось, записанное в строчку
+    printf("%d\n", length);
+    printf("%s", str);
+    //можно отформатировать строчку, чтобы убрать лишнее
+    //formatting_str(&str, length, len);   сделать это в следующей функции то, о чем написанно выше и плюс проверка на валидность того, что написанно 
+    s = ft_formatting(length, str, ap, len); // cделать две функции в одну отправляем signed в другую unsigned, если на конце стоит U
+     //далее нам нужно распарсить str, чтобы узнать флаги и применить их 
 }
 void ft_main(const char * format, int start, int finish, va_list ap) //изменить то, что возвращает функция
 {
@@ -114,6 +210,6 @@ int ft_printf(const char * restrict format, ...) //убрать ft_
 
 int main()
 {
-    ft_printf("hello how are you %d" , 365);
+    ft_printf("hello how are you %-.5lhd" , 365);
     return (0);
 }
